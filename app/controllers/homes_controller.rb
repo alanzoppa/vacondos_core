fa4b725c7fdaca3b733da8fd11c8738ab64d9cf8
C. Alan Zoppa <alan.zoppa@gmail.com>
2016-12-04 20:50:47 -0600

    better join

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,7 +3,7 @@ class HomesController < ApplicationController
   def index
     allowed = params.permit(:line1, :line2, :city, :state, :zip, :country)
 
-    @homes = Home.joins(:lob_address)
+    @homes = Home.joins(:lob_address, :coordinate)
     allowed.each do |key, value|
       @homes = @homes.where("address_#{key} LIKE ?", "#{value}%")
     end 
